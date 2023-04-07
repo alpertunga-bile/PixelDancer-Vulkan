d4df98c09db9fe5b86ba5fcc356db9d162f678d3
alpertunga-bile <kingdom19online@hotmail.com>
2023-04-07 22:19:03 +0300

    Fixed

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Projects/PixelDancer-Vulkan
-# Build directory: D:/Projects/PixelDancer-Vulkan/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
